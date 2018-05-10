b190468bc64b8baf6e23d4a465087a7e902a8511
Dina Hafez <dinahafez.8@gmail.com>
2018-05-10 23:05:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase());
 }
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+}
